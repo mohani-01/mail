683165e5b808f5c5bfd0491ac3420f9094f8ab15
Nahom <nahomohani@gmail.com>
2023-09-26 09:24:23 +0300

    styles.css and adding comment to inbox.js

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -38,16 +38,18 @@ function compose_email() {
       const emailBody = document.querySelector('#compose-body').value;
 
 
-
+      // ask user to confirm that they are sending without subject and body
       if (emailSubject.length == 0 && emailBody.length === 0 && emailRecipients.length > 0 ) {
-        
+
         const txt = "Send this message without a subject and text in the body?";
 
         // ask users if they want to send a message without subject and body
+
         if (confirm(txt) === false) {
           return false;
         }
       }
+      // fetch the data
       fetch('/emails', {
         method: 'POST',
         body: JSON.stringify({
@@ -59,17 +61,21 @@ function compose_email() {
   
       .then(response =>  response.json())
       .then(result => {
-        // get an error and remove div which display error
-        if (result.error) {   
-          if (form.querySelector('#compose-message')) {
-            form.querySelector('#compose-message').remove();
-          }
 
-          // create a new div which display the new error
-          const div = document.createElement('div');
-          div.id = 'compose-message';
-          div.innerHTML = `${result.error} <button class='hide'>X</button>`;
-          form.insertBefore(div, form.children[1])
+        // display the error, 
+        if (result.error) {   
+
+          // change error message if there is one before
+          if (form.querySelector('#compose-message')) {
+            form.querySelector('#compose-message').innerHTML = `${result.error} <button class='hide'>X</button>`;
+
+          } else {
+            // create a new div which display the new error
+            const div = document.createElement('div');
+            div.id = 'compose-message';
+            div.innerHTML = `${result.error} <button class='hide'>X</button>`;
+            form.insertBefore(div, form.children[1])
+          }
           return;
         }
 
@@ -85,7 +91,7 @@ function compose_email() {
       })
       
       .catch(error => {
-        console.log("Error ", error);
+        console.log("Error", error);
         return location.reload();
       })
 
@@ -105,18 +111,22 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
   document.querySelector('#to').style.display = 'none';
 
+  // add a "To" on left side of recipients input area when the user focus on it, change the padding
   document.querySelector('#compose-recipients').addEventListener('focus', () => {
 
-  document.querySelector('#to').style.display = 'table-cell';
-  document.querySelector("#compose-recipients").style.paddingLeft = "0px";
-  document.querySelector("#compose-recipients").placeholder = "";
+    document.querySelector('#to').style.display = 'table-cell';
+    document.querySelector("#compose-recipients").style.paddingLeft = "0px";
+    // remove the placeholder
+    document.querySelector("#compose-recipients").placeholder = "";
   
   });
 
+  // remove the "To" text when the user focus out from the recipient area
   document.querySelector('#compose-recipients').addEventListener('focusout', () => {
 
     document.querySelector('#to').style.display = 'none';
     document.querySelector("#compose-recipients").style.paddingLeft = "20px";
+    // return the place holder
     document.querySelector("#compose-recipients").placeholder = "Recipients";
 
   });
@@ -130,27 +140,37 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(emails => {
     
-    // show the user inv
+    // show the error
     if (emails.error) {
       error(emails.error);
+
     }
+
     emails.forEach(email => {
 
+      // classify each email based on their .read and mailbox value
       const div = document.createElement('div');
-      if ((email.read === true) && (mailbox === 'inbox')) {
+    
+      // inbox and read
+      if ((mailbox === 'inbox') && (email.read === true) ) {
         div.className = 'mailbox read'; 
       } else {
         div.className = 'mailbox';
       }
+      
+      // check for subject of the email if its empty, RE: and othere
       const subject = send_subject(email.subject);
+
       if (mailbox === 'sent')  { 
         div.innerHTML =  `<div class='recipient'>To: ${email.recipients}</div> <div class='subject'>${subject}</div> <div class='time'>${email.timestamp}</div>`;
+
       } else {
         div.innerHTML =  `<div class='recipient'>From: ${email.sender}</div> <div class='subject'>${subject}</div> <div  class='time'>${email.timestamp}</div>`;
       }
 
       document.querySelector('#emails-view').append(div)
 
+      // listen for click for all of the emails
       div.addEventListener('click', () => {
         load_detail(email.id, mailbox)       
       });
@@ -159,7 +179,7 @@ function load_mailbox(mailbox) {
   
   })
  
-  // Error checking    
+  // Error     
   .catch(error => {
     console.log("Error", error);
   });
@@ -174,15 +194,18 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
 
-// View Email
+// View Email in detail
 function load_detail(email_id, mailbox) {
+
   // ask for the data
   fetch(`emails/${email_id}`)
   .then(response => response.json())
   .then(email => {
+    // display error to the user
     if (email.error) {
       error(email.error);
     }
+
     // change the value of read to true
     if(!email.read) {
       change_read(email.id);
@@ -196,16 +219,18 @@ function load_detail(email_id, mailbox) {
     const element = document.createElement('div');
     element.className = 'inbox';
 
-    // body of the page
+    // body of the page replace all \n to <br>
     const body = email.body.replaceAll('\n', '<br>');
 
+    // check if there is a subject or not
     const subject = send_subject(email.subject);
-    let Archive;
+
+    let mailArchive;
 
     if (email.archived == false) {
-       Archive = `<button id='archive' class='btn btn-outline-primary'  data-id=${email.id}>Archive</button>`
+      mailArchive = `<button id='archive' class='btn btn-outline-primary'  data-id=${email.id}>Archive</button>`
     } else {
-       Archive = `<button id='archive' class='btn btn-outline-warning'  data-id=${email.id}>Unarchive</button>`
+      mailArchive = `<button id='archive' class='btn btn-outline-warning'  data-id=${email.id}>Unarchive</button>`
     }
     // add reply and arhieve if the mailbox is inbox     
     if (mailbox === 'inbox' || mailbox == 'archive') {
@@ -219,7 +244,7 @@ function load_detail(email_id, mailbox) {
                               </div>
                               <div class='side-mailbox'> 
                                 <div><button id='reply' class='btn btn-outline-primary' data-id='${email.id}'>Reply</button></div>
-                                <div>${Archive}</div>
+                                <div>${mailArchive}</div>
                               </div>
                           </div>
                           <hr>
@@ -229,7 +254,6 @@ function load_detail(email_id, mailbox) {
         element.innerHTML = `<div class='subject'><h4>${subject}</h4></div>
                             <hr>
                           <div class='top-mailbox'>
-
                               <div>
                                 <div><strong>From: </strong>${email.sender}</div> 
                                 <div><strong>To: </strong>${email.recipients}</div>  
@@ -238,7 +262,6 @@ function load_detail(email_id, mailbox) {
                             </div>
                             <hr>
                             <div>${body}</div>`;
-
       }
     
 
@@ -253,12 +276,15 @@ function load_detail(email_id, mailbox) {
 
     // wait for the user to click the reply, archive button
     element.addEventListener('click', event => {
+
       const button = event.target;
       if (button.id === 'reply') {
         reply(button.dataset.id);
+
       } else {
         if (button.id === 'archive') {
           archive(button.dataset.id, email.archived);
+
         }
       }
     })
@@ -266,6 +292,7 @@ function load_detail(email_id, mailbox) {
   });
 } 
 
+// PUT read to be true
 function change_read(id) {
   fetch( `/emails/${id}` , {
     method: 'PUT',
@@ -277,21 +304,25 @@ function change_read(id) {
 }
 
 
+// reply to user
 function reply(id) {
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
+    // call the compose email
     compose_email()
+
     document.querySelector('#compose-view').querySelector('h3').innerHTML = 'Reply';
 
     document.querySelector('#compose-recipients').value = email.sender;
 
+    // add Re: if it the mail subject doesn't start that way
     if (!email.subject.startsWith('Re:')) {
       document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
     } else {
       document.querySelector('#compose-subject').value = email.subject;
     }
-    // "On Jan 1 2020, 12:00 AM foo@example.com wrote:"
+
 
     document.querySelector('#compose-body').value = `\n\n\n---------------\nOn ${email.timestamp} ${email.sender} wrote: \n${email.body}`;    
 
@@ -299,39 +330,47 @@ function reply(id) {
   
 }
 
+// archive and unarchive email
 function archive(id, value) {
   fetch(`/emails/${id}`, {
     method: 'PUT', 
     body: JSON.stringify({
+
       archived: !value
     })
   })
 
+  // load to inbox page
   location.reload();
 
 }
 
+// display error
 function error(message) {
-  document.querySelector('#emails-view').innerHTML = `<h3>${message}</h3>`;
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#emails-view').innerHTML = `<h3>${message}</h3>`;
   document.querySelector('#detail-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
 }
 
+// make sure there an subject given to email else return no Subject
 function send_subject(emailSubject) {
 
       if (emailSubject.trim().length <= 0 ) {
-        console.log('more than zero character',emailSubject);
-        return "(no Subject)";
+
+        return "(no subject)";
         } else if ( emailSubject.startsWith('Re:')) {
+
           second = emailSubject.split(':', 2)[1];
-
+          // this is for email subjects like "Re: "
           if (second.trim().length <= 0 ) {
-            return "(no Subject)";
+            return "(no subject)";
           }
-
+          // return with for example "RE: Hello"
          return ` ${emailSubject}`;
+
+         // no issues
         } else {
         return emailSubject;
       }
